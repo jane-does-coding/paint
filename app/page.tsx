9ba3a032729514be074a17461ead5b168ed7bce8
Jane <yevheniiasimaka@gmail.com>
2026-03-19 17:49:18 -0600

    idk man, basic ui type shi

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,58 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div className="bg-green-200 w-full h-screen">
+			<img
+				src="/imgs/bg-texture.jpg"
+				className="h-screen w-full fixed top-0 left-0"
+				alt=""
+			/>
+			<img
+				src="/imgs/paint-pallete.jpg"
+				className="h-screen relative z-10"
+				alt=""
+			/>
+			<div className="bg-blue-300/0 left-[32.5vw] top-[5vh] gap-y-[4vh] gap-x-[2vw] px-[3vw] w-[67.5vw] fixed right-0 grid grid-cols-2 overflow-y-auto h-[95vh] pb-[5vh] items-center just">
+				<div className="relative h-fit">
+					<h2 className="absolute bottom-[1vh] text-[2vh] z-5 left-[50%] translate-x-[-50%] text-black italic">
+						Your Next Masterpiece
+					</h2>
+					<h2 className="absolute top-[6vh] text-[8vh] z-5 left-[50%] translate-x-[-50%] text-black italic">
+						+
+					</h2>
+					<h2 className="absolute top-[17vh] text-[2vh] z-5 left-[50%] translate-x-[-50%] text-black italic">
+						Create new project
+					</h2>
+					<img
+						src="/imgs/canvas2-nobg.png"
+						className="w-full h-[30vh] drop-shadow-xl drop-shadow-black/90 relative z-2"
+						alt=""
+					/>
+					<img
+						src="/imgs/canvas2-nobg.png"
+						className="w-full h-[5vh] drop-shadow-xl drop-shadow-black/90 relative z-2 mt-[1vh]"
+						alt=""
+					/>
+				</div>
+				{[1, 2, 3, 4, 5, 6].map((project, i) => (
+					<div className="relative h-fit" key={i}>
+						<h2 className="absolute bottom-[1vh] text-[2vh] z-5 left-[50%] translate-x-[-50%] text-black italic">
+							Lorem Ipsum
+						</h2>
+						<img
+							src="/imgs/canvas2-nobg.png"
+							className="w-full h-[30vh] drop-shadow-xl drop-shadow-black/90 relative z-2"
+							alt=""
+						/>
+						<img
+							src="/imgs/canvas2-nobg.png"
+							className="w-full h-[5vh] drop-shadow-xl drop-shadow-black/90 relative z-2 mt-[1vh]"
+							alt=""
+						/>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
