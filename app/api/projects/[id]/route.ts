9df473a049c8b8ecbff203d5ae3f33cc8bd032d9
Jane <yevheniiasimaka@gmail.com>
2026-04-01 17:53:37 -0600

    vercel fix

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,29 +1,26 @@
 import prisma from "@/app/libs/prismadb";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
 interface Params {
 	params: { id: string };
 }
 
-export async function DELETE(req: Request, { params }: Params) {
+export async function DELETE(req: NextRequest, { params }: Params) {
 	try {
 		const currentUser = await getCurrentUser();
-
 		if (!currentUser) {
 			return NextResponse.json("Unauthorized", { status: 401 });
 		}
 
-		const { id } = await params;
-
-		console.log(id);
+		const { id } = params;
 
 		if (!id) {
 			return NextResponse.json("Project ID is required", { status: 400 });
 		}
 
 		const project = await prisma.project.findUnique({
-			where: { id: id },
+			where: { id },
 		});
 
 		if (!project) {
